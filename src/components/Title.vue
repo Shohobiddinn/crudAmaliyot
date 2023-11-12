d0554a6be937caa0c975f50a2c41d65fa1fdaa3f
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-11-12 09:39:56 +0500

    vuetify install

--- src/components/Title.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Title Vue</h1>
-    </div>
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,19 @@
-
 <template>
-  <!-- <pre v-for="item in product" :key="item?.id">
-    <router-link :to="`/about/${item?.id}`">
-      <img style="width: 100px;height: 100px;" :src="item?.image" alt="">
-    </router-link>
- </pre> -->
- <div>
-  
-  
+  <div>
 
-  <input type="text" ref="input" @keyup="scil">
-  <button @click="clickMe">Click me</button>
-  <ol>
-    <li v-for="item in skils" :key="item">{{ item }}</li>
-  </ol>
-  <!-- <h1 ref="name">Salom Vue</h1> -->
- </div>
+  </div>
 </template>
+
 <script>
-import product from "@/components/product.vue";
 export default {
-  data() {
-    return {
-      skils:[]
-    };
-  },
-  created() {
-  },
-  methods: {
-    scil(event){
-      if(event.key == "," && this.$refs.input){
-        if(this.$refs.input.value.length >= 3){
-          // console.log(this.$refs.input.value);
-          // this.$refs.input.value = this.$refs.input.value.substring(0,this.$refs.input.value.length - 1)
-          // this.skils.push(this.$refs.input.value)
-          // this.$refs.input.value = null;
-          console.log(this.$refs.input.value.substring(0,this.$refs.input.value.length - 1));
-        }
-      }
-    }
- 
-  },
-  components:{
-    product
+  data () {
+    
+
+    return {}
   }
-};
+}
 </script>
+
+<style lang="scss" scoped>
+
+</style>
