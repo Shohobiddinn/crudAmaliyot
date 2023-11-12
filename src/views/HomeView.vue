b7a1efc9f013865c017b92c23c56d9c18f2ccfb2
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-11-12 17:35:13 +0500

    vuetify level up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
- <v-select
-  chips
-  label="Select"
-  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-></v-select>
+  <div class="text-center">
+    <v-btn  style="border-radius: 20px;" color="red-darken-2" @click="snackbar = true"> Open Snackbar </v-btn>
+
+    <v-snackbar  v-model="snackbar" multi-line max-width="200">
+      {{ text }}
+
+      <template v-slot:actions>
+        <v-btn color="red" variant="text" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
+
+
 <script>
 export default {
-  data() {
-    return {
-    };
-  },
+  data: () => ({
+    snackbar: false,
+    text: `I am a multi-line snackbar.\nI can have more than one line. This is another line that is quite long.`,
+  }),
 };
 </script>
-
-<style lang="scss" scoped>
+<style lang="scss" >
+body {
+  background-color: black;
+}
 </style>
