8c5e04712727210ba689c3a20d384282a3b1a065
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-11-13 15:00:56 +0500

    bootstrap level up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <div class="container" style="max-width: 500px">
-      <div class="spinner-border text-black" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
+      <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
+
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded me-2" alt="...">
+      <strong class="me-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+  </div>
+</div>
     </div>
   </div>
 </template>
